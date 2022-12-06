32f15edaae0ff98476f176b86467f84b395ba88b
Jordan Avery <jordan.avery1@gmail.com>
2022-12-06 13:39:24 -0800

    Refactor day 5 to parse the entire input and cover with basic tests

--- day_05/puzzle_01/solution.py
+++ day_05/puzzle_01/solution.py
@@ -1,30 +1,63 @@
+import math
 
-stack_one = ["Q","M","G","C","L"]
-stack_two = ["R","D","L","C","T","F","H","G"]
-stack_three = ["V","J","F","N","M","T","W","R"]
-stack_four = ["J","F","D","V","Q","P"]
-stack_five = ["N","F","M","S","L","B","T"]
-stack_six = ["R","N","V","H","C","D","P"]
-stack_seven = ["H","C","T"]
-stack_eight = ["G","S","J","V","Z","N","H","P"]
-stack_nine = ["Z","F","H","G"]
-
-stacks = [[], stack_one, stack_two, stack_three, stack_four, stack_five, stack_six, stack_seven, stack_eight, stack_nine]
-
-def process_crate_stacks(instructions: list):
+def process_crate_stacks(stacks: list, instructions: list):
+    '''
+    Executes each instruction to move boxes between stacks and prints out the top most box of each stack when done
+    '''
     for instruction in instructions:
-        [_, count, _ ,  source, _, destination] = instruction.split()
-        count = int(count)
-        source = int(source)
-        destination = int(destination)
-        for i in range(count):
+        count, source, destination = parse_single_instruction(instruction)
+        for _ in range(count):
             box = stacks[source].pop()
             stacks[destination].append(box)
-        
     message = [x[-1] for x in stacks[1:]]
     print(''.join(message))
-        
+
+def parse_single_instruction(instruction: str):
+    '''
+    Returns the number of boxes to move from one stack to another based on the instruction format:
+    'move 3 from 3 to 9'
+    '''
+    [_, count, _ ,  source, _, destination] = instruction.split()
+    count = int(count)
+    source = int(source)
+    destination = int(destination)
+    return count, source, destination
+
+
+def create_initial_stacks(stack_rows: list):
+    '''
+    Parser to create a list of lists representing starting package stacks.
+    Creates an empty first element to make indexing easier when processing the move instructions
+    '''
+    stacks = []
+    # Each stack is represented by 4 characters, and each row has a \n which requires taking the floor
+    num_stacks = int(math.floor(len(stack_rows[0])/4))
+
+    # Add an extra empty array here
+    for i in range(0, num_stacks+1):
+        stacks.append([])
+
+    for row in stack_rows:
+        for i in range(0, num_stacks):
+            box = row[i*4 +1]
+            if box != " ":
+                # place the box into a 1-indexed system instead of 0
+                stacks[i+1].insert(0, box)
+
+    return stacks
+    
+def parse_input(input_lines: list):
+    '''
+    Splits the input into two sections: The initial crate stacks and the list of instrucitons.
+    Between the two is an empty line. 
+    There is also a row of indicies after the crate stacks which is skipped.
+    '''
+    split = input_lines.index("\n")
+    return (input_lines[0:split-1], input_lines[split+1:])
+
 if __name__ == "__main__":
     # Assumes a list of lines as input
     with open("../input.txt", "r", encoding="utf-8") as input_file:
-        process_crate_stacks(input_file.readlines())
+        (stack_lines, instructions) = parse_input(input_file.readlines())
+        stacks = create_initial_stacks(stack_lines)
+        process_crate_stacks(stacks, instructions)
--- day_05/tests/test_solution_01.py
+++ day_05/tests/test_solution_01.py
@@ -1,4 +1,33 @@
 import day_05.puzzle_01.solution as s
 
-def test_basic_happy_path():
-    assert(True)
+def test_stack_parser():
+    input_stack_rows = ["    [V]    \n",
+             "[T] [J]    \n",
+             "[C] [S]    \n",
+             "[H] [G] [Z]\n"]
+    stacks = s.create_initial_stacks(input_stack_rows)
+    assert len(stacks) == 4
+    assert stacks[1] == ["H", "C", "T"]
+    assert stacks[2] == ["G", "S", "J", "V"]
+    assert stacks[3] == ["Z"]
+
+def test_input_parser():
+    input_file_contents = ["    [V]    ",
+             "[T] [J]    ",
+             "[C] [S]    ",
+             "[H] [G] [Z]",
+             " 1   2   3",
+             "\n",
+             "move 5 from 8 to 2",
+             "move 2 from 4 to 5",
+             "move 3 from 3 to 9"]
+    stack_lines, instruction_lines = s.parse_input(input_file_contents)
+    assert stack_lines == [
+            "    [V]    ",
+            "[T] [J]    ",
+            "[C] [S]    ",
+            "[H] [G] [Z]"]
+    assert instruction_lines == [
+            "move 5 from 8 to 2",
+            "move 2 from 4 to 5",
+            "move 3 from 3 to 9"]
