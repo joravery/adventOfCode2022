6b6af26535bc1c206f96dc4c4ff5c09ad4a35fa0
Jordan Avery <jordan.avery1@gmail.com>
2022-12-05 21:16:35 -0800

    Fast dirty solution to day 6, no tests

--- day_06/puzzle_01/solution.py
+++ day_06/puzzle_01/solution.py
@@ -1,8 +1,11 @@
-def process_list(lines: list):
-    pass
+def process_signall(signal: str):
+    for i in range(4, len(signal)-4):
+        if len(set(signal[i-4:i])) ==4 :
+            print(f"len(set(signal)): {len(set(signal[i-4:i]))}, signal: {signal[i-4:i]}")
+            print(f"First four characters end at: {i}")
+            return
+
 
 if __name__ == "__main__":
-    # Assumes a list of lines as input
-    pass
     with open("../input.txt", "r", encoding="utf-8") as input_file:
-        pass
+        process_signall(input_file.readlines()[0])
--- day_06/puzzle_02/solution.py
+++ day_06/puzzle_02/solution.py
@@ -1,8 +1,11 @@
-def process_list(lines: list):
-    pass
+def process_signall(signal: str):
+    for i in range(14, len(signal)-14):
+        if len(set(signal[i-14:i])) ==14 :
+            print(f"len(set(signal)): {len(set(signal[i-14:i]))}, signal: {signal[i-14:i]}")
+            print(f"First four characters end at: {i}")
+            return
+
 
 if __name__ == "__main__":
-    # Assumes a list of lines as input
-    pass
     with open("../input.txt", "r", encoding="utf-8") as input_file:
-        pass
+        process_signall(input_file.readlines()[0])
