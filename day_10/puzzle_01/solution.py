f8fa3ea18da5085d63cf4913306333e9a8492c97
Jordan Avery <jordan.avery1@gmail.com>
2022-12-09 22:43:26 -0800

    Minor style fix

--- day_10/puzzle_01/solution.py
+++ day_10/puzzle_01/solution.py
@@ -12,7 +12,7 @@ def process_instructions(instructions: list):
             running_signal_strenth += check_for_special_clock_timing(clock, register_value)
         if command == 'addx':
             add_time = 2
-            while( add_time > 0):
+            while add_time > 0:
                 clock += 1
                 running_signal_strenth += check_for_special_clock_timing(clock, register_value)
                 add_time -= 1
