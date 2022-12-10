88d74bb7d4b3f08b77195135fc00e4f9df349989
Jordan Avery <jordan.avery1@gmail.com>
2022-12-09 16:24:19 -0800

    Consolidated code and tests

--- day_09/puzzle_02/solution.py
+++ day_09/puzzle_02/solution.py
@@ -1,17 +1,24 @@
-def process_list(lines: list):
-    visited_positions = [set((0,0)), set((0,0)), set((0,0)), set((0,0)), set((0,0)), set((0,0)), set((0,0)), set((0,0)), set((0,0)), set((0,0))]
-    knots = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
+def process_list(movements: list, tail_count: int = 9):
+    '''
+    Moves a rope with knots through the movements, updating the position of the trailing knots(tails) after moving the first one(head)
+    '''
+    visited_positions = [set() for _ in range(tail_count+1)]
+    knots = [[0,0] for _ in range(tail_count+1)]
 
+    for i, knot in enumerate(knots):
+        update_visited_positions(knot, visited_positions[i])
 
-    for movement in lines:
+    for movement in movements:
         [direction, magnitude] = movement.split()
         execute_move(direction, int(magnitude), knots, visited_positions)
-    print(f"Total spots visited after executing all commands: {get_total_spots_visited(visited_positions[1])}")
     first_tail_position_count = len(visited_positions[1])
     last_tail_position_count = len(visited_positions[-1])
     return (first_tail_position_count, last_tail_position_count)
 
 def execute_move(direction: str, magnitude: int, knots, visited_positions: list[set]):
+    '''
+    Iteratively moves the head knot, and then updates all following knots (tails) based on the new position
+    '''
     while magnitude > 0:
         update_position(direction, knots[0])
         for i in range(1, len(knots)):
@@ -20,54 +27,72 @@ def execute_move(direction: str, magnitude: int, knots, visited_positions: list[
                 update_visited_positions(knots[i], visited_positions[i])
         magnitude -= 1
 
-
-def update_position(direction: str, pos: list):
+def update_position(direction: str, knot: list):
+    '''
+    Moves a given knot by one position
+    '''
     if direction == "U":
-        pos[0] =  pos[0] + 1
+        knot[0] =  knot[0] + 1
     if direction == "D":
-        pos[0] =  pos[0] - 1
+        knot[0] =  knot[0] - 1
     if direction == "L":
-        pos[1] =  pos[1] - 1
+        knot[1] =  knot[1] - 1
     if direction == "R":
-        pos[1] =  pos[1] + 1
+        knot[1] =  knot[1] + 1
 
-def should_move_tail(head_pos, tail_pos):
-    return abs(head_pos[0] - tail_pos[0]) > 1 or \
-        abs(head_pos[1] - tail_pos[1]) > 1
+def should_move_tail(head, tail):
+    '''
+    A tail (following knot) should only  move if it is more than one position away in any direction
+    '''
+    return abs(head[0] - tail[0]) > 1 or \
+        abs(head[1] - tail[1]) > 1
 
-def update_tail(head_pos, tail_pos):
-    # gross
-    if should_move_diaganolly(head_pos, tail_pos):
-        if head_pos[0] > tail_pos[0]:
-            update_position("U", tail_pos)
-        else:
-            update_position("D", tail_pos)
-        if head_pos[1] > tail_pos[1]:
-            update_position("R", tail_pos)
-        else:
-            update_position("L", tail_pos)
+def update_tail(head: list, tail: list):
+    '''
+    Determines if a knot should move 2 directions (diagonally) or only 1 (laterally) and then updates accordingly
+    '''
+    move_diagonally(head, tail) if should_move_diaganolly(head, tail) else move_laterally(head, tail)
+
+def move_diagonally(head: list, tail: list):
+    '''
+    Moves a following knot in both the horizontal and vertical positions towards the leading knot
+    '''
+    if head[0] > tail[0]:
+        update_position("U", tail)
     else:
-        if head_pos[0] > tail_pos[0]:
-            update_position("U", tail_pos)
-        elif head_pos[0] < tail_pos[0]:
-            update_position("D", tail_pos)
-        elif head_pos[1] > tail_pos[1]:
-            update_position("R", tail_pos)
-        else:
-            update_position("L", tail_pos)
+        update_position("D", tail)
+    if head[1] > tail[1]:
+        update_position("R", tail)
+    else:
+        update_position("L", tail)
 
-def should_move_diaganolly(head_pos, tail_pos):
-    return abs(head_pos[0] - tail_pos[0])+ abs(head_pos[1] - tail_pos[1]) > 2
+def move_laterally(head: list, tail: list):
+    '''
+    Moves a following knot in either the horizontal or vertical positions towards the leading knot
+    '''
+    if head[0] > tail[0]:
+        update_position("U", tail)
+    elif head[0] < tail[0]:
+        update_position("D", tail)
+    elif head[1] > tail[1]:
+        update_position("R", tail)
+    else:
+        update_position("L", tail)
 
-def update_visited_positions(tail_pos: list, visited_position: set):
-    visited_position.add(tuple(tail_pos))
+def should_move_diaganolly(head, tail):
+    '''
+    If a trailing knot is more than 2 positions away from the leading knot it should move diagonally
+    '''
+    return abs(head[0] - tail[0]) + abs(head[1] - tail[1]) > 2
 
-def get_total_spots_visited(visted_map):
-    return len(visted_map)
-
+def update_visited_positions(tail: list, visited_position: set):
+    '''
+    Records a knot's position in a set so all unique positions can be easily counted
+    '''
+    visited_position.add(tuple(tail))
 
 if __name__ == "__main__":
     with open("./input.txt", "r", encoding="utf-8") as input_file:
         FIRST_TAIL, LAS_TAIL = process_list(input_file.readlines())
-    print(f"One tail visited {FIRST_TAIL} unique positions")
+    print(f"The first tail visited {FIRST_TAIL} unique positions")
     print(f"The last of 9 tails visited {LAS_TAIL} unique positions")
--- day_09/tests/test_solution_02.py
+++ day_09/tests/test_solution_02.py
@@ -1,15 +1,6 @@
 import day_09.puzzle_02.solution as s
 
-PUZZLE_TWO_TEST = ["R 5",
-"U 8",
-"L 8",
-"D 3",
-"R 17",
-"D 10",
-"L 25",
-"U 20"]
-
-PUZZLE_ONE_TEST = [
+PROVIDED_PUZZLE_ONE_TEST = [
 "R 4",
 "U 4",
 "L 3",
@@ -20,6 +11,39 @@ PUZZLE_ONE_TEST = [
 "R 2",
 ]
 
+PROVIDED_PUZZLE_TWO_TEST = ["R 5",
+"U 8",
+"L 8",
+"D 3",
+"R 17",
+"D 10",
+"L 25",
+"U 20"]
+
+COMPLEX_TEST_INPUT = ["U 2", "L 2", "R 1", "L 2", "D 1", "U 1", "R 4", "D 1", "U 4", "R 4", "L 3", "U 3", "D 3"]
+
+def test_solution_solves_both_puzzles():
+    assert s.process_list(PROVIDED_PUZZLE_TWO_TEST)[1] == 36
+    assert s.process_list(PROVIDED_PUZZLE_ONE_TEST)[1] == 1
+    assert s.process_list(PROVIDED_PUZZLE_ONE_TEST)[0] == 13
+
 def test_basic_happy_path():
-    assert s.process_list(PUZZLE_TWO_TEST)[1] == 36
-    assert s.process_list(PUZZLE_ONE_TEST)[1] == 1
+    assert s.process_list(COMPLEX_TEST_INPUT)[0] == 12
+
+def test_left_right():
+    assert s.process_list(["L 2", "R 1", "R 1", "R 2"])[0] == 3
+
+def test_up_down():
+    assert s.process_list(["U 2", "D 1", "D 3"])[0] == 3
+
+def test_up_left():
+    assert s.process_list(["U 3", "L 3"])[0] == 5
+
+def test_down_left():
+    assert s.process_list(["D 3", "L 3"])[0] == 5
+
+def test_down_right():
+    assert s.process_list(["D 3", "R 3"])[0] == 5
+
+def test_up_right():
+    assert s.process_list(["U 3", "R 3"])[0] == 5
