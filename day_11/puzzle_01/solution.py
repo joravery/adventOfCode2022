37c5337ea3f45f708f0a618376e922fcbe838603
Jordan Avery <jordan.avery1@gmail.com>
2022-12-11 12:24:12 -0800

    Consolidate into one solution

--- day_11/puzzle_01/solution.py
+++ day_11/puzzle_01/solution.py
@@ -2,64 +2,73 @@ import math
 from day_11.monkey import Monkey
 from day_11.monkey_pack import MonkeyPack
 
+
 def process_list(monkeys: list[Monkey], round_count: int):
     monkey_pack = MonkeyPack(monkeys)
     for i in range(0, round_count):
         monkey_pack.execute_round()
     return monkey_pack.monkey_business(2)
 
-if __name__ == "__main__":
-    monkeys = [
+
+def get_monkey_list(worry_reduction_function: callable):
+    return [
         Monkey(
-        [61],
-        lambda x: x * 11,
-        lambda y:  7 if y % 5 == 0 else 4,
-        lambda z: int(math.floor(z/3))
+            [61],
+            lambda x: x * 11,
+            lambda y:  7 if y % 5 == 0 else 4,
+            worry_reduction_function
         ),
         Monkey(
-        [76, 92, 53, 93, 79, 86, 81],
-        lambda x: x + 4,
-        lambda y:  2 if y % 2 == 0 else 6,
-        lambda z: int(math.floor(z/3))
+            [76, 92, 53, 93, 79, 86, 81],
+            lambda x: x + 4,
+            lambda y:  2 if y % 2 == 0 else 6,
+            worry_reduction_function
         ),
         Monkey(
-        [91, 99],
-        lambda x: x * 19,
-        lambda y: 5 if y % 13 == 0 else 0,
-        lambda z: int(math.floor(z/3))
+            [91, 99],
+            lambda x: x * 19,
+            lambda y: 5 if y % 13 == 0 else 0,
+            worry_reduction_function
         ),
         Monkey(
-        [58, 67, 66],
-        lambda x: x * x,
-        lambda y: 6 if y % 7 == 0 else 1,
-        lambda z: int(math.floor(z/3))
+            [58, 67, 66],
+            lambda x: x * x,
+            lambda y: 6 if y % 7 == 0 else 1,
+            worry_reduction_function
         ),
         Monkey(
-        [94, 54, 62, 73],
-        lambda x: x + 1,
-        lambda y: 3 if y % 19 == 0 else 7,
-        lambda z: int(math.floor(z/3))
+            [94, 54, 62, 73],
+            lambda x: x + 1,
+            lambda y: 3 if y % 19 == 0 else 7,
+            worry_reduction_function
         ),
         Monkey(
-        [59, 95, 51, 58, 58],
-        lambda x: x + 3,
-        lambda y: 0 if y % 11 == 0 else 4,
-        lambda z: int(math.floor(z/3))
+            [59, 95, 51, 58, 58],
+            lambda x: x + 3,
+            lambda y: 0 if y % 11 == 0 else 4,
+            worry_reduction_function
         ),
         Monkey(
-        [87, 69, 92, 56, 91, 93, 88, 73],
-        lambda x: x + 8,
-        lambda y: 5 if y % 3 == 0 else 2,
-        lambda z: int(math.floor(z/3))
+            [87, 69, 92, 56, 91, 93, 88, 73],
+            lambda x: x + 8,
+            lambda y: 5 if y % 3 == 0 else 2,
+            worry_reduction_function
         ),
         Monkey(
-        [71, 57, 86, 67, 96, 95],
-        lambda x: x + 7,
-        lambda y:  3 if y % 17 == 0 else 1,
-        lambda z: int(math.floor(z/3))
+            [71, 57, 86, 67, 96, 95],
+            lambda x: x + 7,
+            lambda y:  3 if y % 17 == 0 else 1,
+            worry_reduction_function
         )
     ]
 
-    with open("./input.txt", "r", encoding="utf-8") as input_file:
-        answer = process_list(monkeys, 20)
-    print(f"Answer is: {answer}")
+if __name__ == "__main__":
+    puzzle_01_monkeys = get_monkey_list(lambda z: int(math.floor(z/3)))
+    puzzle_01_answer = process_list(puzzle_01_monkeys, 20)
+    print(f"Puzzle 1 answer is: {puzzle_01_answer}")
+
+    TEST_FUNCTION_DIVISOR_PRODUCT = 5 * 2 * 13 * 7 * 19 * 11 * 3 * 17
+    puzzle_02_monkeys = get_monkey_list(
+        lambda z: z % TEST_FUNCTION_DIVISOR_PRODUCT)
+    puzzle_02_answer = process_list(puzzle_02_monkeys, 10000)
+    print(f"Puzzle 2 answer is: {puzzle_02_answer}")
--- day_11/tests/test_solution_01.py
+++ day_11/tests/test_solution_01.py
@@ -2,34 +2,65 @@ import math
 import day_11.puzzle_01.solution as s
 from day_11.monkey import Monkey
 
-FIRST_PROVIDED_INPUT = [
-Monkey(
-  [79, 98],
-  lambda x: x * 19,
-  lambda y: 2 if y % 23 == 0 else 3,
-  lambda z: int(math.floor(z/3))
-  ),
-Monkey(
-  [54, 65, 75, 74],
-  lambda x: x + 6,
-  lambda y: 2 if y % 19 == 0 else 0,
-  lambda z: int(math.floor(z/3))
-  ),
-Monkey(
-  [79, 60, 97],
-  lambda x: x * x,
-  lambda y: 1 if y % 13 == 0 else 3,
-  lambda z: int(math.floor(z/3))
-  ),
-Monkey(
-  [74],
-  lambda x: x + 3,
-  lambda y: 0 if y % 17 == 0 else 1,
-  lambda z: int(math.floor(z/3))
-  ),
+PUZZLE_01_TEST = [
+    Monkey(
+        [79, 98],
+        lambda x: x * 19,
+        lambda y: 2 if y % 23 == 0 else 3,
+        lambda z: int(math.floor(z/3))
+    ),
+    Monkey(
+        [54, 65, 75, 74],
+        lambda x: x + 6,
+        lambda y: 2 if y % 19 == 0 else 0,
+        lambda z: int(math.floor(z/3))
+    ),
+    Monkey(
+        [79, 60, 97],
+        lambda x: x * x,
+        lambda y: 1 if y % 13 == 0 else 3,
+        lambda z: int(math.floor(z/3))
+    ),
+    Monkey(
+        [74],
+        lambda x: x + 3,
+        lambda y: 0 if y % 17 == 0 else 1,
+        lambda z: int(math.floor(z/3))
+    ),
+]
+
+TEST_PRODUCT_DIVISOR = 23 * 19 * 13 * 17
+PUZZLE_02_TEST = [
+    Monkey(
+        [79, 98],
+        lambda x: x * 19,
+        lambda y: 2 if y % 23 == 0 else 3,
+        lambda z: z % TEST_PRODUCT_DIVISOR
+    ),
+    Monkey(
+        [54, 65, 75, 74],
+        lambda x: x + 6,
+        lambda y: 2 if y % 19 == 0 else 0,
+        lambda z: z % TEST_PRODUCT_DIVISOR
+    ),
+    Monkey(
+        [79, 60, 97],
+        lambda x: x * x,
+        lambda y: 1 if y % 13 == 0 else 3,
+        lambda z: z % TEST_PRODUCT_DIVISOR
+    ),
+    Monkey(
+        [74],
+        lambda x: x + 3,
+        lambda y: 0 if y % 17 == 0 else 1,
+        lambda z: z % TEST_PRODUCT_DIVISOR
+    ),
 ]
 
 
-
 def test_first_provided_input():
-    assert s.process_list(FIRST_PROVIDED_INPUT, 20) == 10605
+    assert s.process_list(PUZZLE_01_TEST, 20) == 10605
+
+
+def test_puzzle_02():
+    assert s.process_list(PUZZLE_02_TEST, 10000) == 2713310158
