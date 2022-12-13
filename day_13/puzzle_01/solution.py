ad7f081f39385799a9cbc9ce063d41c551ca3248
Jordan Avery <jordan.avery1@gmail.com>
2022-12-12 20:45:22 -0800

    Remove cruft from remaining solution templates

--- day_13/puzzle_01/solution.py
+++ day_13/puzzle_01/solution.py
@@ -2,6 +2,5 @@ def process_list(lines: list):
     pass
 
 if __name__ == "__main__":
-
-    with open("../input.txt", "r", encoding="utf-8") as input_file:
+    with open("./input.txt", "r", encoding="utf-8") as input_file:
         pass
